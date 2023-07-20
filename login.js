8b257df834377f524a4ef112ae298781ba7bc6d7
DanielGonzalezGarzon <daniel.gonzalez.g.14@gmail.com>
2023-07-19 21:18:23 -0500

    Se organizo el ingreso en css y se inicio el .js

---
+++ login.js
@@ -0,0 +1,16 @@
+const usuarios = [
+    {"nombre": "Pipino", "correo":"pipinocorto@algunsitio.com", "pasword":"1234", "monto": 1000000},
+    {"nombre": "Armando Casas", "correo":"casasdebareque@algunlugar.com", "pasword":"1234", "monto": 900000},
+    {"nombre": "Zoila Vaca", "correo":"vacadegar@algunsitio.com", "pasword":"1234", "monto": 2000000},
+    {"nombre": "AlanBrito", "correo":"britodelgado@algunlugar.com", "pasword":"1234", "monto": 800000}
+]
+
+const login = ()=>{ 
+    let correo = document.getElementById("correo").value;
+    let pasword = document.getElementById("pasword").value;
+    let usuarios; 
+    console.log(correo);
+    console.log(pasword);
+     
+}
+
