b39fa804e72e4eef77dab95e8584215339fcbf54
DanielGonzalezGarzon <daniel.gonzalez.g.14@gmail.com>
2023-07-21 22:04:48 -0500

    Se agrego fuciones login y relación, en prueba

--- login.js
+++ login.js
@@ -1,17 +1,30 @@
 const usuarios = [
-    {"nombre": "Pipino", "correo":"pipinocorto@algunsitio.com", "pasword":"1234", "monto": 1000000},
+    {"nombre": "Pipino", "correo":'pipinocorto@algunsitio.com', "pasword":'1234', "monto": 1000000},
     {"nombre": "Armando Casas", "correo":"casasdebareque@algunlugar.com", "pasword":"1234", "monto": 900000},
     {"nombre": "Zoila Vaca", "correo":"vacadegar@algunsitio.com", "pasword":"1234", "monto": 2000000},
     {"nombre": "AlanBrito", "correo":"britodelgado@algunlugar.com", "pasword":"1234", "monto": 800000}
 ]
 /* Construyendo las funciones de ingreso y validacion de las funciones */
 
-const login = ()=>{ 
+function login(){ 
     let correo = document.getElementById("correo").value;
     let pasword = document.getElementById("pasword").value;
-    let usuarios; 
+    let usuario; 
     console.log(correo);
     console.log(pasword);
-     
+
+    let verify = !!usuarios.find(element => element.correo === correo && element.pasword === pasword);
+
+    if (verify){
+        localStorage.setItem("user", JSON.stringify(usuario));
+        window.location='handbank.html';
+    }
+    else{
+        document.getElementById("login-error").classList.remove("none");
+    }       
 }
+function ocultar (){
+    document.getElementById("login-error").classList.add("none");
+}
+
 
